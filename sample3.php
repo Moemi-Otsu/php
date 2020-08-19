29c08e3aba1cdd9dc2a6ee414b01c74b30710474
moemi ohtsu <s0623192@gmail.com>
2020-08-19 22:56:53 +0900

    型と演算子

--- sample3.php
+++ sample3.php
@@ -7,3 +7,27 @@ $string4 = "hi, $string2 \n";
 
 echo $string3, PHP_EOL;
 echo $string4;
+
+$age = 15;
+echo "$ageyears old.", PHP_EOL;
+echo "{$age}years old", PHP_EOL;
+
+$foo = <<<EOI
+ヒアドキュメントでは、このように、
+複数行にわたる文章をそのまま表記することができます。
+
+Tomの年齢は"$age"歳です。
+EOI;
+
+echo $foo;
+
+if (!isset($argv[1])) {
+		exit;
+}
+
+$num = $argv[1];
+if ($num == 100) {
+		echo "num is 100", PHP_EOL;
+} else {
+		echo "num is not 100", PHP_EOL;
+}
