44c93cba8c092273bf7e2cecd534d32ff0c21aef
moemi ohtsu <s0623192@gmail.com>
2020-08-21 23:35:46 +0900

    型演算子

--- sample3.php
+++ sample3.php
@@ -1,33 +1,10 @@
 <?php
 
-$string1 = 'this is string';
-$string2 = "this is string";
-$string3 = 'hi, $string2 \n';
-$string4 = "hi, $string2 \n";
-
-echo $string3, PHP_EOL;
-echo $string4;
-
-$age = 15;
-echo "$ageyears old.", PHP_EOL;
-echo "{$age}years old", PHP_EOL;
-
-$foo = <<<EOI
-ヒアドキュメントでは、このように、
-複数行にわたる文章をそのまま表記することができます。
-
-Tomの年齢は"$age"歳です。
-EOI;
-
-echo $foo;
-
-if (!isset($argv[1])) {
-		exit;
+class SomeClass
+{
 }
+$a = new SomeClass();
 
-$num = $argv[1];
-if ($num == 100) {
-		echo "num is 100", PHP_EOL;
-} else {
-		echo "num is not 100", PHP_EOL;
+if ($a instanceof SomeClass) {
+		echo '$a は SomeClass のインスタンスです', PHP_EOL;
 }
