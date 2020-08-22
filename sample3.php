24bfd18c8eff5f566db6273405dac2a501bf93bc
moemi ohtsu <s0623192@gmail.com>
2020-08-22 23:41:51 +0900

    変数

--- sample3.php
+++ sample3.php
@@ -8,3 +8,12 @@ $a = new SomeClass();
 if ($a instanceof SomeClass) {
 		echo '$a は SomeClass のインスタンスです', PHP_EOL;
 }
+
+$var = 1;
+var_dump(isset($var));
+$var = null;
+var_dump(isset($var));
+var_dump($var);
+unset($var);
+var_dump(isset($var));
+var_dump($var);
