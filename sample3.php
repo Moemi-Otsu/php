a561b2f2222db76524026ca3e3df333c0d0eae1f
moemi ohtsu <s0623192@gmail.com>
2020-08-23 22:37:01 +0900

    変数のキャスト注意

--- sample3.php
+++ sample3.php
@@ -17,3 +17,13 @@ var_dump($var);
 unset($var);
 var_dump(isset($var));
 var_dump($var);
+
+echo (float)'15foo', PHP_EOL;
+echo (float)'15.33foo', PHP_EOL;
+// eは10のべき乗を表す
+echo (float)'1e2foo', PHP_EOL;
+echo (float)'foo', PHP_EOL;
+
+if ('0.0' == '0') {
+		echo ' "0.0"と"0"は等しいです';
+}
