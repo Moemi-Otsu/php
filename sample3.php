5f5b6d11be0664159838d0c83d2e9d436cf1341b
moemi ohtsu <s0623192@gmail.com>
2020-09-02 23:57:43 +0900

    型と演算子

--- sample3.php
+++ sample3.php
@@ -25,5 +25,21 @@ echo (float)'1e2foo', PHP_EOL;
 echo (float)'foo', PHP_EOL;
 
 if ('0.0' == '0') {
-		echo ' "0.0"と"0"は等しいです';
+		echo ' "0.0"と"0"は等しいです', PHP_EOL;
 }
+
+$a = 10;
+echo $a, PHP_EOL;
+echo $a = 11, PHP_EOL;
+
+$age = 15;
+$tom = 'Tom is ' . $age . ' years old.';
+echo $tom, PHP_EOL;
+
+// 整数を直接文字列として連結したいときは、スペースを開ける必要がある。
+// Bobは浮動小数てんと文字列が演算子なしに連続して記述されていると見なされている。
+$tom = 'Tom is ' . 15 . ' years old.';
+
+$a = 10;
+$a += 1;
+echo $a, PHP_EOL;
