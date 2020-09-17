460249526b02f975ae9e2085f0932021d8d15075
moemi ohtsu <s0623192@gmail.com>
2020-09-17 23:53:56 +0900

    配列

--- array.php
+++ array.php
@@ -59,3 +59,14 @@ $fruits_tajigen = array(
 foreach ($fruits_tajigen as $name => $value) {
 		echo "$name は１つ {$value['price']}円で、{$value['count']}個です。", PHP_EOL;
 }
+
+$a = array('a' => 1, 'b' => 3, 'c' => 5);
+$b = array('a' => 1, 'c' => 5, 'b' => 3);
+$c = array('a' => 1, 'b' => 2);
+
+var_dump($a == $b);
+var_dump($a === $b);
+
+var_dump($a + $c);
+
+var_dump($c + $a);
