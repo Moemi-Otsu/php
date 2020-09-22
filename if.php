19b6731b0f3170a00d1ee2d707e44cc19df76390
moemi ohtsu <s0623192@gmail.com>
2020-09-22 23:52:16 +0900

    制御構造と関数

--- if.php
+++ if.php
@@ -4,8 +4,14 @@ if (true) if (true) {
 		echo 1, PHP_EOL;
 }
 
-$i = 0;
-while ($i < 10) {
-		echo $i, PHP_EOL;
-		$i++;
-}
+//$i = 0;
+//while ($i < 10) {
+//		echo $i, PHP_EOL;
+//		$i++;
+//}
+
+$a = 0;
+do {
+		echo $a, PHP_EOL;
+		$a++;
+} while ($a < 10);
