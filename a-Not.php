8004c57f22d9b2fc2d64d9948e232982e5edb513
moemi ohtsu <s0623192@gmail.com>
2020-11-01 21:48:13 +0900

    AtcoderA問題とか

---
+++ a-Not.php
@@ -0,0 +1,11 @@
+<?php
+
+//fscanf(STDIN, "%d", $a);
+
+$a = 1;
+
+if ($a == 0) {
+		echo 1;
+} elseif ($a == 1) {
+		echo 0;
+}
---
+++ techAcademy.php
@@ -0,0 +1,10 @@
+<?php
+
+function outputName($name) {
+//		$name = "山田";
+		echo "私の名前は" . $name . "です。";
+}
+
+outputName("山田");
+outputName("鈴木");
+outputName("秋田");
