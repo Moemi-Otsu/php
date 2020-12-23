a6a0648fc7df1a701383d7fa5dcbe6765e5f10f6
moemi ohtsu <s0623192@gmail.com>
2020-12-23 21:37:54 +0900

    22. ユーザー定義関数その2

--- udemy/22_basic.php
+++ udemy/22_basic.php
@@ -16,8 +16,36 @@ test();
 
 $comment = 'コメント2';
 
+// 引数のinputと、変数の名前は違ってOK
+// inputと、処理の中で使っている変数は同じ必要がある
 function getComment($string) {
 		echo $string;
 }
 
 getComment($comment);
+
+// インプット引数なし
+// アウトプット戻り値あり
+
+function getNumberOfComment() {
+		return 5;
+}
+
+// 変数の中に関数を入れることもできる
+$commentNumber = getNumberOfComment();
+
+// 下記は両方5を表示
+echo getNumberOfComment();
+echo $commentNumber;
+
+
+// インプット引数２つ
+// アウトプット戻り値あり
+
+function sumPrice($int1, $int2) {
+		$int3 = $int1 + $int2;
+		return $int3;
+}
+
+$total = sumPrice(3, 5);
+echo $total;
