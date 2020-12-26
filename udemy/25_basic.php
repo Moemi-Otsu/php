333fce44ce277995e351931edbb3e06381deea1d
moemi ohtsu <s0623192@gmail.com>
2020-12-26 21:17:39 +0900

    25. 文字列関数 explode, preg_match, substr

--- udemy/25_basic.php
+++ udemy/25_basic.php
@@ -5,3 +5,22 @@
 $str_2 = '指定文字列で、分割します';
 
 var_dump(explode('、', $str_2));
+
+// implode
+
+// 正規表現（preg_match）
+// 文字列かどうか
+// 数字かどうか
+// 郵便番号とか
+// メールアドレスか
+
+$str_3 = '特定の文字列が含まれるか確認する';
+echo preg_match('/文字列/',$str_3);
+
+
+
+// 指定文字列から文字列を取得する（substr）
+
+echo substr('abcde', 1);
+// マルチバイト
+echo mb_substr('かきくけこ', 2);
