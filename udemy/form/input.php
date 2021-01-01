16c75eccec8d4915eafa210ee345ac9551a07c4f
moemi.otsu <moemi.otsu@NPC-173A.local>
2021-01-01 17:57:44 +0900

    form

---
+++ udemy/form/input.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<head></head>
+<body>
+
+<form method="GET" action="input.php">
+  氏名
+  <input type="text" your_name="yout_name">
+  <input type="submit" value="送信">
+</form>
+</body>
+</html>
