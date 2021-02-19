f132dedf9ab1b98ae8f6cfbc6028612a1750f4c2
moemi ohtsu <s0623192@gmail.com>
2021-02-19 23:53:15 +0900

    null

--- 319.php
+++ 319.php
@@ -9,5 +9,14 @@ function no_return_func()
 $null_value = no_return_func(); // nullが代入される
 
 if ($null_value === null) {
-    echo 'nullです';
+    echo 'nullです', PHP_EOL;
 }
+
+$var = 1;
+var_dump(isset($var)); // true. 変数$varは定義されている
+$var = null;
+var_dump(isset($var)); //false. 変数$varはnullのため、セットされていないとされる
+var_dump($var); // null. 変数$varはnull
+unset($var);
+var_dump(isset($var)); // false. 変数$varはunset()されている。
+var_dump($var); // unsetされたためNoticeが発生。値としてはnull
