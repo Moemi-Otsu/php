0154af6cbf4689fb17f43d9ff43e84e413d59481
moemi ohtsu <s0623192@gmail.com>
2021-02-24 23:51:33 +0900

    キャスト

---
+++ string-cast2.php
@@ -0,0 +1,10 @@
+<?php
+$age = 15;
+$tom = 'Tom is ' . $age . ' years old.';
+echo $tom, PHP_EOL;
+
+$tom2 = 'Tom is ' . 15 . ' years old.';
+echo $tom2, PHP_EOL;
+
+$tom3 = 'Tom is ' . 16. ' years old.';
+echo $tom3, PHP_EOL;
