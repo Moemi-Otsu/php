f064349b91f77af7e42261c6a53ae24e8a3ac80d
moemi ohtsu <s0623192@gmail.com>
2021-04-09 23:39:51 +0900

    abstruct

---
+++ abstract.php
@@ -0,0 +1,14 @@
+<?php
+
+abstract class Employee
+{
+    abstract public function work();
+}
+
+class Programmer extends Employee
+{
+    public function work()
+    {
+        // TODO: Implement work() method.
+    }
+}
