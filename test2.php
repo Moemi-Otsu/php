6200a76bbdd4f54c941c5f48266d01163c8b1914
moemi ohtsu <s0623192@gmail.com>
2021-08-27 23:58:48 +0900

    クラス変数とインスタンス変数

---
+++ test2.php
@@ -0,0 +1,26 @@
+<?php
+
+class Foo {
+  public $instance_variable;
+  
+  public static $class_variable;
+  
+  public function showClassVariable()
+  {
+    var_dump(self::$class_variable);
+  }
+}
+
+$foo1 = new Foo();
+$foo2 = new Foo();
+
+$foo1->instance_variable = "あああ\n";
+$foo2->instance_variable = "いいい\n";
+
+echo $foo1->instance_variable;
+echo $foo2->instance_variable;
+
+Foo::$class_variable = "ううう";
+
+$foo1->showClassVariable();
+$foo2->showClassVariable();
