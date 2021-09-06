680d6bc61d0d7540d960e745063f95079e451ab2
moemi ohtsu <s0623192@gmail.com>
2021-09-06 23:12:28 +0900

    インスタンスがいつ破棄されるのかについて

---
+++ argument_reference8.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+function makecoffee($types = array("cappuccino"), $coffeeMaker = NULL)
+{
+  $device = is_null($coffeeMaker) ? "hands" : $coffeeMaker;
+  
+  return "Making a cup of ".join(", ", $types)." with $device\n";
+}
+
+echo makecoffee();
+echo makecoffee(array("cappuccino", "lavazza"), "teapot");
