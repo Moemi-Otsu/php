a756b7683d6a855e4f1e5d98cba0f18c08115cfc
moemi ohtsu <s0623192@gmail.com>
2021-11-21 19:12:55 +0900

    simple class

--- SimpleClass.php
+++ SimpleClass.php
@@ -3,5 +3,18 @@ declare(strict_types=1);
 
 class SimpleClass
 {
+  public $var = "a default value";
   
+  public function displayVar()
+  {
+    echo $this->var;
+  }
 }
+
+$instance = new SimpleClass();
+
+$assigned = $instance;
+$reference =& $instance;
+
+//echo $instance->var = '$assigned will have this value';
+echo $instance->var;
