ecc10f760d2551072f6bf2eddbb1b717eab6d380
moemi.otsu <moemi.otsu@stafes.com>
2022-01-05 23:38:02 +0900

    ReflectionClass

--- Container.php
+++ Container.php
@@ -7,23 +7,53 @@ class Container
     /** @var array<string, string> */
     private $definitions = [];
 
+    // 連想配列にキーとクラスの完全修飾子をセットするメソッド
     public function define(string $id, string $className): void
     {
         $this->definitions[$id] = $className;
     }
 
+    // DIコンテナ
+    // 連想配列にidがセットされていたら、対象のキーのオブジェクトをnewする
+    // DIとは、オブジェクトの注入
     public function get(string $id)
     {
-        if (isset($this->definitions[$id])) {
-            $className = $this->definitions[$id];
+        // 定義されている ID であればそれに紐づいたクラス名を、そうでない場合は $id 自体をクラス名として扱う
+        $className = $this->definitions[$id] ?? $id;
+
+        // そのクラス名を元に ReflectionClass クラスのインスタンスを生成
+        $ref = ReflectionClass($className);
+
+        // Constructor の取得
+        $constructor = $ref->getConstructor();;
+
+        // Constructor が存在しない場合は、引数なしでインスタンス化
+        if ($constructor === null) {
             return new $className;
         }
 
-        return null;
+        $args = [];
+        // getParameters() コンストラクタの引数の情報を全て取得
+        foreach ($constructor->getParameters() as $parameter) {
+            // 引数の型情報を取得
+            $type = $parameter->getType();
+            $args[] = $this->get($type->getName());
+        }
+
+        return new $className(...$args);
     }
 }
 
-class Foo {}
+class Bar {}
+
+class Foo {
+    public $bar;
+
+    public function __construct(Bar $bar)
+    {
+        $this->bar = $bar;
+    }
+}
 
 $container = new Container();
 
